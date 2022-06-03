fa651bdbd61db37694dceb017d4237c34917099c
GiovannaNiz <giovanna.santana20@etec.sp.gov.br>
2022-06-03 17:51:12 -0300

    um

---
+++ app/build/generated/data_binding_trigger/debug/com/example/android/navigation/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.android.navigation;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/navigation/GameFragment.java
@@ -0,0 +1,110 @@
+package com.example.android.navigation;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010!\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001:\u0001\u001fB\u0005\u00a2\u0006\u0002\u0010\u0002J&\u0010\u0014\u001a\u0004\u0018\u00010\u00152\u0006\u0010\u0016\u001a\u00020\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u00192\b\u0010\u001a\u001a\u0004\u0018\u00010\u001bH\u0016J\b\u0010\u001c\u001a\u00020\u001dH\u0002J\b\u0010\u001e\u001a\u00020\u001dH\u0002R \u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0006\u0010\u0007\"\u0004\b\b\u0010\tR\u001a\u0010\n\u001a\u00020\u000bX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\f\u0010\r\"\u0004\b\u000e\u0010\u000fR\u000e\u0010\u0010\u001a\u00020\u0011X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0012\u001a\u00020\u0011X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0013\u001a\b\u0012\u0004\u0012\u00020\u000b0\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006 "}, d2 = {"Lcom/example/android/navigation/GameFragment;", "Landroidx/fragment/app/Fragment;", "()V", "answers", "", "", "getAnswers", "()Ljava/util/List;", "setAnswers", "(Ljava/util/List;)V", "currentQuestion", "Lcom/example/android/navigation/GameFragment$Question;", "getCurrentQuestion", "()Lcom/example/android/navigation/GameFragment$Question;", "setCurrentQuestion", "(Lcom/example/android/navigation/GameFragment$Question;)V", "numQuestions", "", "questionIndex", "questions", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "randomizeQuestions", "", "setQuestion", "Question", "app_debug"})
+public final class GameFragment extends androidx.fragment.app.Fragment {
+    private final java.util.List<com.example.android.navigation.GameFragment.Question> questions = null;
+    @org.jetbrains.annotations.NotNull()
+    public com.example.android.navigation.GameFragment.Question currentQuestion;
+    @org.jetbrains.annotations.NotNull()
+    public java.util.List<java.lang.String> answers;
+    private int questionIndex = 0;
+    private final int numQuestions = 0;
+    private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.android.navigation.GameFragment.Question getCurrentQuestion() {
+        return null;
+    }
+    
+    public final void setCurrentQuestion(@org.jetbrains.annotations.NotNull()
+    com.example.android.navigation.GameFragment.Question p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.String> getAnswers() {
+        return null;
+    }
+    
+    public final void setAnswers(@org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    private final void randomizeQuestions() {
+    }
+    
+    private final void setQuestion() {
+    }
+    
+    public GameFragment() {
+        super();
+    }
+    
+    @kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010 \n\u0002\b\t\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0086\b\u0018\u00002\u00020\u0001B\u001b\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\f\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005\u00a2\u0006\u0002\u0010\u0006J\t\u0010\u000b\u001a\u00020\u0003H\u00c6\u0003J\u000f\u0010\f\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005H\u00c6\u0003J#\u0010\r\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\u000e\b\u0002\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005H\u00c6\u0001J\u0013\u0010\u000e\u001a\u00020\u000f2\b\u0010\u0010\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0011\u001a\u00020\u0012H\u00d6\u0001J\t\u0010\u0013\u001a\u00020\u0003H\u00d6\u0001R\u0017\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u0014"}, d2 = {"Lcom/example/android/navigation/GameFragment$Question;", "", "text", "", "answers", "", "(Ljava/lang/String;Ljava/util/List;)V", "getAnswers", "()Ljava/util/List;", "getText", "()Ljava/lang/String;", "component1", "component2", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+    public static final class Question {
+        @org.jetbrains.annotations.NotNull()
+        private final java.lang.String text = null;
+        @org.jetbrains.annotations.NotNull()
+        private final java.util.List<java.lang.String> answers = null;
+        
+        @org.jetbrains.annotations.NotNull()
+        public final java.lang.String getText() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final java.util.List<java.lang.String> getAnswers() {
+            return null;
+        }
+        
+        public Question(@org.jetbrains.annotations.NotNull()
+        java.lang.String text, @org.jetbrains.annotations.NotNull()
+        java.util.List<java.lang.String> answers) {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final java.lang.String component1() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final java.util.List<java.lang.String> component2() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final com.example.android.navigation.GameFragment.Question copy(@org.jetbrains.annotations.NotNull()
+        java.lang.String text, @org.jetbrains.annotations.NotNull()
+        java.util.List<java.lang.String> answers) {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        @java.lang.Override()
+        public java.lang.String toString() {
+            return null;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object p0) {
+            return false;
+        }
+    }
+}
